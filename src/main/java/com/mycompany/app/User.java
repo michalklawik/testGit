0fca1df8e45903fbd4237bb68a254d1d3b32a38a
michalklawik <WejhiT261f>
2018-09-25 10:48:39 +0200

    Added User Class

---
+++ src/main/java/com/mycompany/app/User.java
@@ -0,0 +1,58 @@
+package com.mycompany.app;
+
+import java.util.Objects;
+
+public class User {
+
+    int id;
+    String name;
+    String surName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surName, user.surName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surName);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                '}';
+    }
+}
